f4e57c6309b32167c1758b418a33168cde6a73b2
Jimmy Brannon <user.email>
2018-05-19 13:46:45 -0500

    remove shift on api data

--- src/services/redditAPI.js
+++ src/services/redditAPI.js
@@ -1,13 +1,13 @@
 const axios = require('axios');
 
-const URL = 'https://www.reddit.com/r/Bossfight/top/.json?sort=top&t=month&limit=100';
+const URL = 'https://www.reddit.com/r/Bossfight/.json?sort=top&t=month&limit=100';
 
 
 function fetchReddit() {
     return axios.get(URL)
     .then(res => res.data.data.children)
     .then(children => {
-      children.shift();
+      // children.shift();
       return children.filter(child => child.data.preview.images[0].source.url).map(child => {
         let {id, title: name, author: creator, ups: offense, num_comments: defense, preview} = child.data;
         let photo = preview.images[0].variants.gif ? preview.images[0].variants.gif.source.url : preview.images[0].source.url
